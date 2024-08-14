f21f8d9544deefa6576a4dc3267dc31376bb45ea
Nilton <niltonprudys@gmail.com>
2024-08-13 21:47:41 -0300

    feat: profile pages

--- src/pages/OurTeam/components/LookScreen/LookScreen.tsx
+++ src/pages/OurTeam/components/LookScreen/LookScreen.tsx
@@ -1,15 +1,43 @@
-import { Title, YelowBox } from "./stryle"
-import { Link } from "react-router-dom"
+import { Typography } from "@mui/material";
+import { ProfileBox, StyledImages, TeamBox, Title, YelowBox } from "./stryle";
 
-export const LookScren = () => {
+export const LookScreen = () => {
     return (
         <>
             <YelowBox>
                 <Title variant="h3">
                     Nosso time
                 </Title>
-                
+                <TeamBox>
+                    <ProfileBox>
+                        <StyledImages src="/Marilia.png" alt="Foto de Marília Guimarães" />
+                        <Typography variant="h6" component="h2">
+                            Marília Guimarães
+                        </Typography>
+                        <Typography variant="h6" component="h2">
+                            Sócia fundadora
+                        </Typography>
+                    </ProfileBox>
+                    <ProfileBox>
+                        <StyledImages src="/Vanusa.png" alt="Foto de Vanusa Vilarinho" />
+                        <Typography variant="h6" component="h2">
+                            Vanusa Vilarinho
+                        </Typography>
+                        <Typography variant="h6" component="h2">
+                            Sócia fundadora
+                        </Typography>
+                    </ProfileBox>
+                    <ProfileBox>
+                        <StyledImages src="/Consultores.png" alt="Imagem dos consultores" />
+                        <Typography variant="h6" component="h2">
+                            Consultores parceiros
+                        </Typography>
+                        <Typography variant="h6" component="h2">
+                            Colaboradores
+                        </Typography>
+                    </ProfileBox>
+                </TeamBox>
             </YelowBox>
         </>
-    )
-}
+    );
+};
--- src/pages/OurTeam/components/LookScreen/stryle.tsx
+++ src/pages/OurTeam/components/LookScreen/stryle.tsx
@@ -1,26 +1,62 @@
-import styled from "@emotion/styled";
-import { Box, Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 
 export const YelowBox = styled(Box)`
-    width: 100vw;
     min-height: 100vh;
     background-color: #f1bb06;
     color: white;
     display: flex;
-    justify-content: space-between;
-    padding: 50px;
-    align-items: center;
-    flex-wrap: wrap;
+    padding: 100px;
+    flex-direction: column;
+    justify-content: space-around;
 
     @media (max-width: 768px) {
         padding: 20px;
-        flex-direction: column;
-        justify-content: center;
         text-align: center;
+        justify-content: center;
     }
 `;
 
 export const Title = styled(Typography)`
     color: #0B314A;
     font-weight: bold;
+    margin-bottom: 20px;
+
+    @media (max-width: 768px) {
+        font-size: 24px;
+        margin-bottom: 15px;
+    }
+`;
+
+export const StyledImages = styled("img")`
+    width: 200px;
+    border-radius: 50%;
+
+    @media (max-width: 768px) {
+        width: 150px;
+    }
+`;
+
+export const ProfileBox = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+
+    @media (max-width: 768px) {
+        gap: 10px;
+        padding: 10px;
+    }
+`;
+
+export const TeamBox = styled(Box)`
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        align-items: center;
+    }
 `;
